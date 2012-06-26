f056e77c108d8ecd068da966cb0fdfe2e5be9faa
Jonathan M. Altman <jonathan@async.io>
2012-06-26 10:53:00 -0400

    cleaned out original, unused jade template files.  link display now working, fixed page title variable

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ var Link = require('../models/Link.js');
 
 exports.index = function(req, res){
 	Link.find(function(error, links) {
-	  res.render('index', { title: 'Express', links: links });
+	  res.render('index', { title: 'Links', links: links });
 	});
 };
