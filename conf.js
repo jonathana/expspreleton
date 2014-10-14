80a3c91a2fd56bba172b862b9c3f0a38f2b7ca91
jonathana <jonathan.a@gmail.com>
2014-10-14 12:30:11 -0400

    Regenerated google secret key
    To make sure this continues to work out of the box, the updated secret key for google needs to be in here.

--- conf.js
+++ conf.js
@@ -2,6 +2,6 @@ module.exports = {
   sessionSecret: 'YouHadBetterNotStoreYourSecretThisWay'
   , google: {
         clientId: '892524237598-ej68aubms87ub4sl15h8419deg9uerln.apps.googleusercontent.com'
-      , clientSecret: 'So6ljiz2aYGAxyV51NhjEVeq'
+      , clientSecret: '7ZQzskb6t8GH14QDiWSEyNq-'
     }
 };
